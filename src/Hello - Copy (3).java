69831089218dee954225cbe05119e56bfc96719c
bob <bob@anil>
2018-09-13 17:46:58 +0530

    adding ant project file

--- src/Hello - Copy (3).java
+++ src/Hello - Copy (3).java
@@ -1,7 +1,7 @@
-class Hello
+class Hello2
 {
    public static void main(String args[])
    {
-      System.out.println("Hello World");
+      System.out.println("Hello World from class2");
    }
 }
--- src/Hello - Copy.java
+++ src/Hello - Copy.java
@@ -1,7 +1,7 @@
-class Hello
+class Hello1
 {
    public static void main(String args[])
    {
-      System.out.println("Hello World");
+      System.out.println("Hello World from AT");
    }
 }
